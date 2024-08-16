c08e17466cd47627e8e99a5ff99e879c683e739d
Atlit <atlit@outlook.com>
2024-08-17 00:44:14 +0200

    Create submit button

--- javascript/reincarnation/reincarnation.js
+++ javascript/reincarnation/reincarnation.js
@@ -2,6 +2,18 @@ import g from '../genericPanel.js'
 import data from './data.js'
 import logic from './logic.js'
 
-const run = () => {
-    reincarnation = g.create('div', 'reincarnation')
+const run = (parent) => {
+    g.appendChildren(parent, createSubmitButton(parent, 'submit-button'))
+}
+
+const createSubmitButton = (parent, ...classes) => {
+    const button = g.create('button')
+    button.textContent = 'Find your new race!'
+    button.addEventListener('click', function () { logic.execute(parent) })
+    g.addClasses(button, classes)
+    return button
+}
+
+export default {
+    run,
 }
