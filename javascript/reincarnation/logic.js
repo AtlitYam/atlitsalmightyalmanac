7a37d21c68d177132620df53778f716f4afdfde8
Atlit <atlit@outlook.com>
2024-08-17 01:05:38 +0200

    Clean up console log

--- javascript/reincarnation/logic.js
+++ javascript/reincarnation/logic.js
@@ -3,7 +3,6 @@ import data from './data.js'
 const execute = () => {
     const reincarnationTable = data.reincarnationTable
     const randomIndex = Math.floor(Math.random() * reincarnationTable.length)
-    console.log(reincarnationTable[randomIndex])
     return reincarnationTable[randomIndex]
 }
 
